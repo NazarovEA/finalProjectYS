d31c42301ce5506c151266e6eab898a22f98b371
NazarovEA <nk-nazarovEA@yandex.ru>
2023-11-10 23:20:13 +0000

    1 загрузка

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://4c460f6d-9c71-43f8-9691-8ac702c7dddb.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+POST_CREATE_ORDER = "/api/v1/orders"
+GET_ORDER_NUMBER = "/api/v1/orders/track"
